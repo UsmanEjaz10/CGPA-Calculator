fcb1dfadce3e5e6e43b22d2cd73d18b96fd56589
Usman <alie25708@gmail.com>
2023-04-16 22:36:24 +0500

    Add Button created (Adds textView + EditText views dynamically)

--- app/src/main/java/com/usmanejaz/cgpa_calculator/MainActivity.java
+++ app/src/main/java/com/usmanejaz/cgpa_calculator/MainActivity.java
@@ -27,9 +27,10 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static int i = 0;
-    public static int j = i+10;
+    public static int i = 1;
+    public static int j = i+20;
     EditText course, grade;
+    TextView name;
     LinearLayout ll;
     Double gpatotal =10.0;
     Double chtotal =3.0;
@@ -57,7 +58,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-                if(i == 4) {
+                if(i == 10) {
                     Snackbar.make(view, "Max course limit reached", Snackbar.LENGTH_LONG)
                             .setAction("Action", null).show();
                 }
@@ -78,10 +79,10 @@ public class MainActivity extends AppCompatActivity {
                 EditText gpa1, ch1;
                 Double gpa = 0.0;
                 Double ch = 0.0;
-                for (int k = 0; k <i; k++){
+                for (int k = 1; k <i; k++){
 
                     gpa1 = findViewById(k);
-                    ch1 = findViewById(k+10);
+                    ch1 = findViewById(k+20);
                     String text2 = ch1.getText().toString();
                     String text = gpa1.getText().toString();
                     double gpaDouble = Double.parseDouble(text);
@@ -102,18 +103,41 @@ public class MainActivity extends AppCompatActivity {
 
     public void addButton() {
         LinearLayout layout = (LinearLayout)findViewById(R.id.ll);
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        params.setMargins(20,20,20,20);
+
+
         ll = new LinearLayout(this);
         ll.setOrientation(LinearLayout.HORIZONTAL);
+        name = new TextView(this);
         course = new EditText(this);
         grade = new EditText(this);
+
+        name.setGravity(View.TEXT_ALIGNMENT_GRAVITY);
         grade.setGravity(View.TEXT_ALIGNMENT_GRAVITY);
-        course.setGravity(View.TEXT_ALIGNMENT_GRAVITY);
-        course.setHint("GPA" + i);
-        grade.setHint("Credit hrs." + i);
+      //  course.setGravity(View.TEXT_ALIGNMENT_GRAVITY);
+
+        name.setPadding(5,0 , 5, 5);
+
+
+        name.setText("Course" + i);
+        name.setTextSize(20);
+
+        course.setHint("GPA");
+        course.setTextSize(20);
+
+        grade.setHint("Credit hrs.");
+        grade.setTextSize(20);
+
+        name.setLayoutParams(params);
+        course.setLayoutParams(params);
+        grade.setLayoutParams(params);
+
         grade.setId(j);
         course.setId(i);
         i++;
         j++;
+        ll.addView(name);
         ll.addView(course);
         ll.addView(grade);
         layout.addView(ll);
