f461447cb14742bda0e823313affdef236e20f2d
Usman <alie25708@gmail.com>
2023-04-17 02:25:56 +0500

    Removed action bar from both screens

--- app/src/main/java/com/usmanejaz/cgpa_calculator/MainActivity.java
+++ app/src/main/java/com/usmanejaz/cgpa_calculator/MainActivity.java
@@ -41,7 +41,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
 
         /*FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
--- app/src/main/java/com/usmanejaz/cgpa_calculator/Splash.java
+++ app/src/main/java/com/usmanejaz/cgpa_calculator/Splash.java
@@ -2,13 +2,50 @@ package com.usmanejaz.cgpa_calculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
+
+import android.os.*;
 
 public class Splash extends AppCompatActivity {
 
+    TextView logo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+
+
+        animate();
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+
+
+                Intent intent = new Intent(Splash.this, MainActivity.class);
+                startActivity(intent);
+            }
+
+
+
+        }, 5000);
+
+
+
+    }
+
+    public void animate(){
+        logo = findViewById(R.id.logo);
+        Animation move = AnimationUtils.loadAnimation(this, R.anim.animation_splash);
+
+        logo.startAnimation(move);
+
     }
 }
